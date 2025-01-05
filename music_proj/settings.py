15663f894f48d220bcaef3d942acb94f0e7a9d49
VKalaitanov <kalaytanov93@gmail.com>
2025-01-05 16:42:24 +0300

    add media

--- music_proj/settings.py
+++ music_proj/settings.py
@@ -139,8 +139,8 @@ STATIC_ROOT = '/app/static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
 # ]
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
