2980892c2288a275f5ef211691c7061206e181c4
VKalaitanov <kalaytanov93@gmail.com>
2025-01-05 16:21:51 +0300

    add hosts

--- music_proj/settings.py
+++ music_proj/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/music_project/static/'
+STATIC_ROOT = '/app/static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
 # ]
