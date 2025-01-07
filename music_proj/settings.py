333d4f2fdb8043846817db888a23202b3c2cd825
VKalaitanov <kalaytanov93@gmail.com>
2025-01-07 20:14:37 +0300

    поля убраны

--- music_proj/settings.py
+++ music_proj/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['90.156.169.33', 'igor-uspehov.com', 'localhost', '127.0.0.1']
 
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'music_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': env('POSTGRES_HOST'),
-        'PORT': env('POSTGRES_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('POSTGRES_DB'),
+#         'USER': env('POSTGRES_USER'),
+#         'PASSWORD': env('POSTGRES_PASSWORD'),
+#         'HOST': env('POSTGRES_HOST'),
+#         'PORT': env('POSTGRES_PORT'),
+#     }
+# }
 
 
 # Password validation
@@ -135,11 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/app/static/'
+# STATIC_ROOT = '/app/static/'
+STATIC_ROOT = '/static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
 # ]
-MEDIA_ROOT = '/app/media'
+# MEDIA_ROOT = '/app/media'
+MEDIA_ROOT = '/media/'
 MEDIA_URL = 'media/'
 DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600
 
